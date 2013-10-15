160583f5b17843452ccce0a9bb319bae95d1b3d2
Paul Sheridan <paul.sheridan@me.com>
2013-10-14 20:34:57 -0700

    Copied Vending Machine

---
+++ SimpleVendingMachineWithClass/SimpleVendingMachine/PurchasePrice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleVendingMachine
+{
+    class PurchasePrice
+    {
+        // This constructor sets the purchase price to zero 
+        public PurchasePrice()
+        {
+            
+        }
+ 
+        // This constructor allows a new purchase price to be set by the user 
+        public PurchasePrice(int initialPrice)
+        {
+            
+        }
+ 
+        // This property gets and sets the value the purchase price. 
+        public int Price
+    }
+}
